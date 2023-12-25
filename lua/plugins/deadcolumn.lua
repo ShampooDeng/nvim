2b80c5b44ce0cf83b046db2dc6928234c5bf0a0e
ShampooDeng <lwxkkdy@foxmail.com>
2023-12-26 00:28:22 +0800

    feat: add deadcolumn.lua for managing line width

---
+++ lua/plugins/deadcolumn.lua
@@ -0,0 +1,30 @@
+local linewidth_configs = {
+	-- filetype, width
+	{'pyton', 79},
+}
+
+local function setup_colorcolumn_per_filetype(type, width)
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = type,
+		callback = function ()
+			vim.opt_local.colorcolumn = {width}
+		end
+	})
+end
+
+local function colorcolumn_per_filetype()
+	for _, lw_config in ipairs(linewidth_configs) do
+		setup_colorcolumn_per_filetype(lw_config[1], lw_config[2])
+	end
+end
+
+return {
+	'Bekaboo/deadcolumn.nvim',
+	config = function ()
+		vim.opt.colorcolumn = {80}
+		colorcolumn_per_filetype()
+		require("deadcolumn").setup({
+			scope = "visible"
+		})
+	end
+}
