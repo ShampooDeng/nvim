0c181d90cfd9a2543d64d72e64101ef862561caa
ShampooDeng <lwxkkdy@foxmail.com>
2024-01-28 23:53:15 +0800

    feat: open help tag vertically by default in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,11 +7,26 @@ return {
 	},
 	config = function ()
 		local builtin = require('telescope.builtin')
+		local actions = require('telescope.actions')
+
+		local function my_help_tag()
+			builtin.help_tags({
+				-- More details can be found in telescope's wiki
+				-- https://github.com/nvim-telescope/telescope.nvim/blob/master/developers.md#replacing-actions
+				attach_mappings = function (prompt_bufnr, _)
+					actions.select_default:replace(function ()
+						actions.select_vertical({prompt_bufnr})
+					end)
+					return true
+				end
+			})
+		end
+
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
 		vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		vim.keymap.set('n', '<leader>fh', my_help_tag, {})
 		vim.keymap.set('n', ':', builtin.commands, {})
 		vim.keymap.set('n', '<leader>h', builtin.command_history, {})
 		vim.keymap.set('n', '<C-p>', builtin.builtin, {})
