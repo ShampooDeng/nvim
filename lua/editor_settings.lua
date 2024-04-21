12f10968e7d17863cbd113a8ac536065ee8965a5
ShampooDeng <lwxkkdy@foxmail.com>
2024-04-21 11:20:37 +0800

    feat: enable lsp.inlay_hint and add corresponding keymap
    1. enable vim.lsp.inlay_hint for nvim higher than 0.10
    2. <c-k> for toggling native inlay_hint

--- lua/editor_settings.lua
+++ lua/editor_settings.lua
@@ -15,3 +15,24 @@ opt.smartcase = true
 -- TODO: Shared clipboard through nvim instances.
 opt.clipboard:append({"unnamedplus"})
 opt.ttimeoutlen = 100
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local bufnr = args.buf
+		local file_type, _ = vim.filetype.match({ buf = 0 })
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		local nvim_version = vim.version()
+		if client ~= nil and client.server_capabilities.inlayHintProvider and nvim_version.minor >= 10 then
+			vim.keymap.set(
+				{ "n", "i" },
+				"<C-k>",
+				"<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<cr>"
+			)
+			vim.api.nvim_cmd({
+				cmd = 'lua',
+				args = { "vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())" }
+			}, {})
+		end
+	end
+})
