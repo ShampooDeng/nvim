2e3494f81603d62c274e8e7cf0ec461fa856c7a3
ShampooDeng <lwxkkdy@foxmail.com>
2024-04-21 11:26:10 +0800

    feat: add config for mason

--- lua/plugins/lspconf.lua
+++ lua/plugins/lspconf.lua
@@ -68,6 +68,21 @@ local function config_neodev()
 	})
 end
 
+local function config_mason()
+	require("mason").setup({
+		github = {
+			---@since 1.0.0
+			-- The template URL to use when downloading assets from GitHub.
+			-- The placeholders are the following (in order):
+			-- 1. The repository (e.g. "rust-lang/rust-analyzer")
+			-- 2. The release version (e.g. "v0.3.0")
+			-- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
+			download_url_template = "https://github.com/%s/releases/download/%s/%s",
+			-- download_url_template = "https://mirror.ghproxy.com/https://github.com/%s/releases/download/%s/%s",
+		},
+	})
+end
+
 return {
 	PLUGIN_PROXY .. "VonHeikemen/lsp-zero.nvim",
 	branch = 'v2.x',
@@ -79,6 +94,7 @@ return {
 			build = function()
 				pcall(vim.api.nvim_command, 'MasonUpdate')
 			end,
+			config = config_mason,
 		},
 		{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 		-- Autocompletion
