272db52e1c8b2fa236615ec034708c59a1673c99
ShampooDeng <lwxkkdy@foxmail.com>
2024-08-22 00:58:29 +0800

    feat: new im-switch plugin
    Switch to drop-stones/im_switch.nvim

--- lua/plugins/im_switch.lua
+++ lua/plugins/im_switch.lua
@@ -1,15 +1,32 @@
 return {
-	"Kicamon/im-switch.nvim",
-	config = function()
-		require("im-switch").setup({
-			txt = {
-				'*.txt',
-				'*.md',
-			},
-			code = {},
-			en = "fcitx5-remote -c",
-			zh = "fcitx5-remote -o",
-			check = "fcitx5-remote",
-		})
-	end
+	"drop-stones/im-switch.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	event = "VeryLazy",
+	opts = {
+		windows = {
+			enabled = true,
+		},
+		linux = {
+			enabled = true,
+			default_im = "xkb:us::eng",
+			obtain_im_command = { "ibus", "engine" },
+			set_im_command = { "ibus", "engine" },
+		},
+	}
+	-- "Kicamon/im-switch.nvim",
+	-- config = function()
+	-- 	require("im-switch").setup({
+	-- 		txt = {
+	-- 			'*.txt',
+	-- 			'*.md',
+	-- 		},
+	-- 		code = {},
+	-- 		-- en = "fcitx5-remote -c",
+	-- 		-- zh = "fcitx5-remote -o",
+	-- 		-- check = "fcitx5-remote",
+	-- 		en = "ibus engine xkb:us::eng",
+	-- 		zh = "ibus engine rime",
+	-- 		check = "ibus engine",
+	-- 	})
+	-- end
 }
