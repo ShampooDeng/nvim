56a761d533bfff0996c3fd84116a3582e617284f
ShampooDeng <lwxkkdy@foxmail.com>
2024-08-22 01:00:54 +0800

    chore: update keymaps for rustacenvim

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,11 +1,11 @@
--- vim.keymap.set('n', '<leader>b', function()
--- 	vim.cmd.RustLsp { 'runnables' }
--- end)
 vim.keymap.set('n', '<leader>r', function()
 	vim.cmd.RustLsp { 'runnables' }
 end)
 vim.keymap.set('n', '<leader>b', function()
-	vim.cmd.Cargo { 'build' }
+	vim.cmd.Cargo { 'run' }
+end)
+vim.keymap.set('n', '<leader>c', function()
+	vim.cmd.Cargo { 'check' }
 end)
 vim.keymap.set('n', '<leader>t', function()
 	vim.cmd.RustLsp { 'testables' }
